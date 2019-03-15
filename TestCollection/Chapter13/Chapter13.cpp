c51e332b37a1a516f2b8e531b5bf7c3108896051
Christian Ødum Jensen <chr_jensen@hotmail.com>
2019-03-15 14:29:10 +0100

    For Chapter 13

--- TestCollection/Chapter13/Chapter13.cpp
+++ TestCollection/Chapter13/Chapter13.cpp
@@ -20,6 +20,33 @@ void Chapter13NS::Chapter13::HandleSection13_1()
 {
 }
 
+class S
+{
+public:
+	int j;
+	string s;
+	double d;
+	S(int i, string s1, double d1) {
+		j = i;
+		s = s1;
+		d = d1;
+	}
+};
+
 void Chapter13NS::Chapter13::HandleSection13_2()
 {
+	auto p1 = make_shared<S>(1, "Acme Inc", 1.2);
+	auto p2 = make_unique<S>(2, "Ron Weasley", 3.2);
+
+	cout << '\n';
+
+	cout << p1->d << '\n';
+	cout << p2 << '\n';
+	cout << p2->j << '\n';
+
+	auto p3 = move(p2);
+	cout << p3 << '\n';
+	cout << p3->s.c_str() << '\n';
+	cout << p2 << '\n';
+
 }
